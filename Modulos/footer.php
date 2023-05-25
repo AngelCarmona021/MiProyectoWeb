83354afa9d66dad8aa7c08bb4f6181e95050e4df
AngelCarmona021 <134582065+AngelCarmona021@users.noreply.github.com>
2023-05-25 00:27:13 -0600

    Add files via upload

---
+++ Modulos/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+
+    <strong>Copyright &copy; 2022 <a href="https://
+    www.SIDVYC.com" target="_blank">SIDVYC</a>.
+    </strong>
+
+    Todos los derechos reservados.
+
+
+</footer>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+require_once "Controladores/plantilla.controlador.php";
+require_once "Controladores/usuarios.controlador.php";
+require_once "Controladores/categorias.controlador.php";
+require_once "Controladores/productos.controlador.php";
+require_once "Controladores/clientes.controlador.php";
+require_once "Controladores/ventas.controlador.php";
+
+
+require_once "Modelos/usuarios.modelo.php";
+require_once "Modelos/categorias.modelo.php";
+require_once "Modelos/productos.modelo.php";
+require_once "Modelos/clientes.modelo.php";
+require_once "Modelos/ventas.modelo.php";
+
+$plantilla = new ControladorPlantilla();
+$plantilla -> ctrPlantilla();
